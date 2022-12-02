139a2fade56418dfca92c475d311d0f7816e75c0
João Paulo <joaopaulo4911@gmail.com>
2022-12-02 16:16:00 -0300

    Created ASCII art and modified install_flameshot.sh file

--- setup.sh
+++ setup.sh
@@ -1,1 +1,29 @@
 #!/bin/bash
+
+echo "                                                    __ ";
+echo " _____     _ _        _ _ _     _                  |  |";
+echo "|  |  |___| | |___   | | | |___| |___ ___ _____ ___|  |";
+echo "|     | -_| | | . |  | | | | -_| |  _| . |     | -_|__|";
+echo "|__|__|___|_|_|___|  |_____|___|_|___|___|_|_|_|___|__|";
+echo "                                                       ";
+
+echo "               ,     \    /      ,                ";
+echo "              / \    )\__/(     / \               ";        
+echo "             /   \  (_\  /_)   /   \              "; 
+echo " ___________/_____\__\@  @/___/_____\____________ ";
+echo "|                    |\../|                      |";
+echo "|                     \VV/                       |";
+echo "|   This script will help with the process of    |";
+echo "|  installing and configuring applications on a  |";
+echo "|  new Linux system (Ubuntu and derivatives).    |";
+echo "|     https://github.com/j0tz/my_workstation     |";
+echo "|              ~ Written by @j0tz ~              |";
+echo "|________________________________________________|";
+echo " |    /\ /              \\\              \ /\    | ";
+echo " |  /   V                ))              V   \  | ";
+echo " |/     \`               //               ´     \| ";
+echo " \`                     //                      ´ ";
+echo "                       V                          ";
+
+# Software installation
+./software/install_flameshot.sh
