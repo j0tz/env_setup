d60c749458d8efab9c790899f2a398e55fab7ef6
João Paulo <joaopaulo4911@gmail.com>
2022-12-05 21:41:01 -0300

    Separated ASCII art into a specific file for code organization and readability purposes

---
+++ ascii_art.sh
@@ -0,0 +1,25 @@
+echo "                                                    __ ";
+echo " _____     _ _        _ _ _     _                  |  |";
+echo "|  |  |___| | |___   | | | |___| |___ ___ _____ ___|  |";
+echo "|     | -_| | | . |  | | | | -_| |  _| . |     | -_|__|";
+echo "|__|__|___|_|_|___|  |_____|___|_|___|___|_|_|_|___|__|";
+echo "                                                       ";
+
+echo "               ,     \    /      ,                ";
+echo "              / \    )\__/(     / \               ";        
+echo "             /   \  (_\  /_)   /   \              "; 
+echo " ___________/_____\__\@  @/___/_____\____________ ";
+echo "|                    |\../|                      |";
+echo "|                     \VV/                       |";
+echo "|    This script will automate the process of    |";
+echo "|  installing and configuring programs on a new  |";
+echo "|  Linux system (Ubuntu 22.04 and derivatives).  |";
+echo "|     https://github.com/j0tz/my_workstation     |";
+echo "|              ~ Created by @j0tz ~              |";
+echo "|________________________________________________|";
+echo " |    /\ /              \\\              \ /\    |";
+echo " |  /   V                ))              V   \  | ";
+echo " |/     \`               //               ´     \|";
+echo " \`                     //                       ´";
+echo "                       V                          ";
+echo "                                                  ";
--- setup.sh
+++ setup.sh
@@ -5,31 +5,7 @@ if [ "$(lsb_release -i -s)" != "Ubuntu" ] || [ "$(lsb_release -c -s)" != "jammy"
 	exit 1
 fi
 
-echo "                                                    __ ";
-echo " _____     _ _        _ _ _     _                  |  |";
-echo "|  |  |___| | |___   | | | |___| |___ ___ _____ ___|  |";
-echo "|     | -_| | | . |  | | | | -_| |  _| . |     | -_|__|";
-echo "|__|__|___|_|_|___|  |_____|___|_|___|___|_|_|_|___|__|";
-echo "                                                       ";
-
-echo "               ,     \    /      ,                ";
-echo "              / \    )\__/(     / \               ";        
-echo "             /   \  (_\  /_)   /   \              "; 
-echo " ___________/_____\__\@  @/___/_____\____________ ";
-echo "|                    |\../|                      |";
-echo "|                     \VV/                       |";
-echo "|    This script will automate the process of    |";
-echo "|  installing and configuring programs on a new  |";
-echo "|  Linux system (Ubuntu 22.04 and derivatives).  |";
-echo "|     https://github.com/j0tz/my_workstation     |";
-echo "|              ~ Created by @j0tz ~              |";
-echo "|________________________________________________|";
-echo " |    /\ /              \\\              \ /\    |";
-echo " |  /   V                ))              V   \  | ";
-echo " |/     \`               //               ´     \|";
-echo " \`                     //                       ´";
-echo "                       V                          ";
-echo "                                                  ";
+./ascii_art.sh
 
 echo "WARNING: By proceeding with the execution of the script, unwanted changes may be made to your machine."
 read -r -p "Do you want to continue? [Y/n] " continue
