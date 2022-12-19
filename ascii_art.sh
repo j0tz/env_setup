8ea9803ec7b0eab7be1f66763c12bc21ee0c768b
João Paulo <joaopaulo4911@gmail.com>
2022-12-18 22:40:32 -0300

    Updated repository link

--- ascii_art.sh
+++ ascii_art.sh
@@ -19,7 +19,7 @@ echo          "|                      \VV/                      |";
 echo          "|      This script automates the process of      |";
 echo          "| configuring the Linux environment Ubuntu 22.04 |";
 echo          "|     after clean installation of the system.    |";
-echo          "|    https://github.com/j0tz/environment_setup   |";
+echo          "|        https://github.com/j0tz/env_setup       |";
 echo          "|              ~ Created by @j0tz ~              |";
 echo          "|________________________________________________|";
 echo          " |    /\ /              \\\              \ /\    |";
